68cfbc87ab560d78d8ff28f49dcdeb0f2e154f9f
jstan32 <jstan32@student.monash.edu>
2016-05-19 17:07:46 +1000

    Update addlocationPage.js

--- assignment2/code/js/addlocationPage.js
+++ assignment2/code/js/addlocationPage.js
@@ -8,48 +8,28 @@
 document.getElementById("headerBarTitle").textContent = "Add Location";
 
 // Displays initial map 
+if(!document.getElementById("mapsapi"))
+{
+  var script = document.createElement("script");
+  script.setAttribute("src","https://maps.googleapis.com/maps/api/js?v=3");
+  script.setAttribute("id", "mapsapi");
+	var bodyNode = document.getElementsByTagName("body")[0];
+	bodyNode.appendChild(script);
+}
+
 var map;
+
+document.getElementById("outputArea").innerHTML = '<div id="map" style="height: 600px; width: 100%;">Loading map...</div>';
+
+setTimeout(initMap,1000);
 
 function initMap()
 {
-  var monashuniposition = { lat: -37.912, lng: 145.131};
-  map = new.google.maps.Map(document.getElementById("map"),
+  // centered at monash uni clayton
+  var monashClaytonPosition = { lat: -37.912, lng: 145.131};
+  map = new google.maps.Map(document.getElementById("map")),
   {
-    zoom: 16,
-    center : monashuniposition
-  });
-  var geocoder = new google.maps.Geocoder();
-  
-  document.getElementById('submit').addEventListener('click', function()
-  {
-    geocodeAddress(geocoder,map);
-  }
-}
-// Add Location textbox (geocode)
-// Has to be added to HTML/ edited to suit requirements 
-function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('address').value;
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
+    zoom:16,
+    center : monashClaytonPosition
   });
 }
-
-// Location 
-
-// For change in Text fields,(onchange property)
-var outputAreaRef = document.getElementById("outputArea");
-
-var html = '<input type="text" oninput="fieldValueChanged(this.value)" onchange="fieldValueEntered(this.value)" /><br />';
-html += '<div id="message1" ></div>';
-html += '<div id="message2" ></div>';
-outputAreaRef.innerHTML = html;
-
-// onclick button for AddLocation
